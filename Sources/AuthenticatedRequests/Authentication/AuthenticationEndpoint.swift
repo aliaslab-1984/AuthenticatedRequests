20480bc670f47deff0d90def93aa3e5d271bb0ca
Enrico Bonaldo <asclepix@yahoo.it>
2023-06-06 12:00:00 +0200

    avoid empty querystring

--- Sources/AuthenticatedRequests/Authentication/AuthenticationEndpoint.swift
+++ Sources/AuthenticatedRequests/Authentication/AuthenticationEndpoint.swift
@@ -59,6 +59,9 @@ public extension Dictionary where Value == String, Key == String {
     
     var urlEncoded: Data? {
         return self.compactMap { touple in
+            
+            guard !touple.value.isEmpty else { return nil }
+            
             return "\(touple.key)=\(touple.value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? touple.value)"
         }.joined(separator: "&").data(using: .utf8)
     }
